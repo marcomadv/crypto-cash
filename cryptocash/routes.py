25e1f8d85fef1bbc1ae529c52b758c64db64065b
marcomadv <imarcoma@gmail.com>
2023-05-17 14:24:22 +0200

    primeros cambios

--- cryptocash/routes.py
+++ cryptocash/routes.py
@@ -1,5 +1,14 @@
 from cryptocash import app
+from flask import render_template
 
 @app.route("/")
 def index():
-    return "Proyecto final"
+    return render_template ("index.html")
+
+@app.route("/purchase")
+def compra():
+    return render_template("purchase.html")
+
+@app.route("/status")
+def estado():
+    return render_template("/status.html")
---
+++ data/create_tabla.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "tabla" (
+	"id"	INTEGER NOT NULL,
+	"date"	TEXT NOT NULL,
+	"time"	TEXT NOT NULL,
+	"moneda_from"	TEXT NOT NULL,
+	"cantidad_from"	REAL NOT NULL,
+	"moneda_to"	TEXT NOT NULL,
+	"cantidad_to"	REAL NOT NULL,
+	PRIMARY KEY("id")
+)
