374d8d1297d5bbc886833622687b9c4da587133e
marcomadv <imarcoma@gmail.com>
2023-05-20 00:42:06 +0200

    primeras pruebas con formulario y algunas funciones en models nuevas

--- cryptocash/models.py
+++ cryptocash/models.py
@@ -5,7 +5,7 @@ import requests
 ########################## BBDD ################################
 
 def view_all():
-    con = sqlite3.connect("data/crypto.sqlite") # conectar con base de datos
+    con = sqlite3.connect("data/movimientos.sqlite") # conectar con base de datos
     cur = con.cursor() #cursor para poder ejecutar las querys
 
     res = cur.execute("select * from tabla;")  #query o peticion a la base de datos
@@ -28,12 +28,45 @@ def view_all():
     return lista_diccionario
 
 def insert(registroForm):
-    con = sqlite3.connect("data/crypto.sqlite")
+    con = sqlite3.connect("data/movimientos.sqlite")
     cur = con.cursor()
     res = cur.execute("INSERT INTO tabla(date,time,moneda_from,cantidad_from,moneda_to,cantidad_to) VALUES(?,?,?,?,?,?)", registroForm) #hacer insert en base datos de los datos añadidos en formulario
     
     con.commit() #validacion de registros
     con.close() #cierre de conexion
+
+def coinsFrom(): #monedas from
+    con = sqlite3.connect("data/movimientos.sqlite")
+    con.row_factory = lambda cursor, row: row[0]
+    cur = con.cursor()
+    res = cur.execute("SELECT DISTINCT moneda_from from tabla") 
+    datos = res.fetchall()
+    con.close()
+    return datos
+
+def sumFrom(moneda): # suma de monedas from
+    con = sqlite3.connect("data/movimientos.sqlite")
+    cur = con.cursor()
+    res = cur.execute(f"SELECT sum (cantidad_from) from tabla where moneda_from={moneda}")
+    resultado = res.fetchall()
+    con.close()
+    return resultado
+
+def sumTo(moneda):
+    con = sqlite3.connect("data/movimientos.sqlite")
+    cur = con.cursor()
+    res = cur.execute(f"SELECT sum (cantidad_to) from tabla where moneda_to={moneda}")
+    resultado = res.fetchall()
+    con.close()
+    return resultado
+
+'''
+def balanceCrypto(moneda):
+    resultado = sumFrom(moneda) - sumTo(moneda)
+    return resultado
+'''
+    
+
 
 
 ############################## API #########################
@@ -43,11 +76,26 @@ def getAllRates(apikey,moneda=""): # todos los rates de esta moneda
     datos = r.json()
     return datos
 
-def getExchange(apikey,cripto=""): #cambio de una cripto a EUR
-    r = requests.get(f"https://rest.coinapi.io/v1/exchangerate/{cripto}/EUR?apikey={apikey}")
+def getExchange(apikey,crypto,crypto2): #valor de una cripto o moneda en otra cripto o moneda
+    r = requests.get(f"https://rest.coinapi.io/v1/exchangerate/{crypto}/{crypto2}?apikey={apikey}")
     datos = r.json()
-    return datos
+    rate = datos['rate']
+    return rate
 
+def purchaseCrypto(eur,crypto): #cuanto valor se puede comprar de esa cripto con 'X' EUR.
+    rate = getExchange(crypto)
+    total = eur / rate
+    return total
+    
+def changeCrypto(apikey,q,crypto1, crypto2): # Funcion docu API, cambiar valor de una cripto comprada por el de otra cripto
+    r = requests.get(f"https://rest.coinapi.io/v1/exchangerate/{crypto1}/{crypto2}?apikey={apikey}")
+    datos = r.json()
+    rate = datos['rate']
+    cambio = int(rate)* q
+    return cambio
+
+
+'''
 def getAllCoins(apikey):
 
     lista_criptos = []
@@ -67,7 +115,7 @@ def getAllCoins(apikey):
         else:
             lista_no_criptos.append(item["asset_id"])
 
-'''
+
 class Exchange: 
     def __init__(self, cripto):
         self.moneda_cripto = cripto
--- cryptocash/routes.py
+++ cryptocash/routes.py
@@ -9,19 +9,29 @@ from cryptocash.models import *
 def index():
     datos = view_all()
     #cripto = getAllRates(APIKEY,moneda="BTC")
-    cripto = getExchange(APIKEY,cripto="ETH")
+    #cripto = getExchange(APIKEY,crypto="ETH")
 
-    return render_template("index.html", data=datos, crypto=cripto)
+    return render_template("index.html", data=datos)
 
 @app.route("/purchase", methods=['GET','POST'])
 def compra():
+
+    cryptos=["EUR","ETH","BNB","ADA","DOT","BTC","USDT","XRP","SOL","MATIC"]
+    coins = coinsFrom()
+    cointo2 = "Hola"
     if request.method == "GET":
         pass
 
     if request.method == "POST":
-        pass
-
-    return render_template("purchase.html")
+        print(request.form)
+        coinfrom = request.form["coinFrom"]
+        cointo = request.form["coinTo"]
+        print(f"Coin from {coinfrom} coin to {cointo}")
+        rate = getExchange(APIKEY,coinfrom,cointo)
+        print(f"The exchange is: {rate}")
+        cointo2 = "Adios"
+    
+    return render_template("purchase.html",monedas=coins, criptos=cryptos,resultado=cointo2)
 
 @app.route("/status")
 def estado():
